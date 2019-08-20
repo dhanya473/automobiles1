3ae67126fee40e03c34da06b287a394d5ac6166b
dhanya <dhanyajayachandran4@gmail.com>
2019-08-20 12:09:30 +0530

    automobile1 script commited

---
+++ src/test/java/com/scripts/Automobile_test_method.java
@@ -0,0 +1,13 @@
+package com.scripts;
+
+import org.testng.annotations.Test;
+
+public class Automobile_test_method
+{
+	@Test
+	void call()
+	{
+	System.out.println("welcome to automobiles world");
+
+}
+}
